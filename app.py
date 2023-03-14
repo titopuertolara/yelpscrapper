f3b22328a0bf3b3518d9ff1b1b2e4d9b283cccf2
aepuertol <andrespuertolara@gmail.com>
2023-03-13 21:08:10 -0500

    :(

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from dash import Dash, dcc, html, Input, Output,State,ctx, dash_table
+from dash import Dash, dcc, html, Input, Output,State,ctx, dash_table,ALL
 import os
 import pandas as pd
 from kmodes.kmodes import KModes
@@ -25,48 +25,71 @@ app.layout=html.Div([
         html.Div(id='url-div',children=[dcc.Input(id='yelp-url'
             ,placeholder='eg: https://www.yelp.com/biz/applebees-grill-bar-san-francisco'
             ,style={'width':'100%'})],style={'width':'40%','display':'inline-block'}),
-        html.Div([dcc.Dropdown(id='n-pages',options=[{'label':str(i)+' pages','value':i} for i in range(1,11)],value=10)]
+        html.Div([dcc.Dropdown(id='n-pages',options=[{'label':str(i)+' pages','value':i} for i in range(1,11)],value=1)]
             ,style={'width':'10%','display':'inline-block','position':'absolute','margin-left':'1%'}),
          html.Div([html.Button('Scrap!',id='scrp-btn',n_clicks=0)],style={'display':'inline-block','margin-left':'12%'}),
-        html.Div(id='scrapper-results')
+        dcc.Loading(id='table-loading',children=[html.Div(id='scrapper-results')],type='circle',style={'margin-top':'20%'}),
+        html.Div(id='dummy-div'),
+        dcc.Store(id='reviews-store')
 
     ])
 ])
 
 @app.callback(Output('scrapper-results','children'),
-                    [Input('scrp-btn','n_clicks'),
-                    State('yelp-url','value'),
-                    State('n-pages','value')])
+              Output('reviews-store','data'),
+                [Input('scrp-btn','n_clicks'),
+                State('yelp-url','value'),
+                State('n-pages','value')])
 def get_results(n_clicks,url,n_pages):
 
-
+     reviews_data_table=""
+     reviews=pd.DataFrame()
      if 'scrp-btn'==ctx.triggered_id:
         #print(url,n_pages)
         n_pages=int(n_pages)
-
-        if n_pages==1:
-            res=scrapper.multi_pages(url,n_pages)
-        else:
-            
-            res=scrapper.multi_pages(url,n_pages)
         
-        reviews=scrapper.create_yelp_df(res)
 
-        reviews_data_table=dash_table.DataTable(
-                                id='reviews-table',
-                                style_table={'height':'500px','overflowY':'auto','overflowX':'auto','width':'auto'},
-                                style_header={'backgroundColor': '#393F56','fontWeight': 'bold','color':'white'},
-                                style_cell={'textAlign':'left','textOverflow': 'ellipsis','overflow': 'hidden','maxWidth': 0},
-                                export_format='csv',
-                                page_size=10,
-                                columns=[{'name':i,'id':i} for i in reviews.columns],
-                                data=reviews.to_dict('records')
-            )
+        try:
+
+            if n_pages==1:
+                res=scrapper.multi_pages(url,n_pages)
+            else:
+                
+                res=scrapper.multi_pages(url,n_pages)
+            
+            reviews=scrapper.create_yelp_df(res)
+
+            reviews_data_table=dash_table.DataTable(
+                                    id={'type':'datatable','index':'reviews-table'},
+                                    style_table={'height':'500px','overflowY':'auto','overflowX':'auto','width':'auto'},
+                                    style_header={'backgroundColor': '#393F56','fontWeight': 'bold','color':'white'},
+                                    style_cell={'textAlign':'left','textOverflow': 'ellipsis','overflow': 'hidden','maxWidth': 0},
+                                    export_format='csv',
+                                    page_size=20,
+                                    columns=[{'name':i,'id':i} for i in reviews.columns],
+                                    data=reviews.to_dict('records'),
+                                    tooltip_data=[{column: 
+                                        {
+                                            'value': str(value), 'type': 'markdown'} for column, value in row.items()
+                                        } for row in reviews.to_dict('records')
+                                    ],
+                                    row_selectable="single"
+
+                )
+        except:
+            reviews_data_table="something's wrong, make sure this is a yelp link"
 
 
-        return reviews_data_table
+
+     return reviews_data_table,reviews.to_json()
     
-    
+@app.callback(Output('dummy-div','children'),
+              [Input({'type':'datatable','index':ALL},'derived_virtual_selected_rows'),
+              Input('reviews-store','data')])  
+def show_insights(table_index,stored_reviews):
+    reviews_df=pd.read_json(stored_reviews)
+    print(reviews_df)
+    return ''
 
 
 
